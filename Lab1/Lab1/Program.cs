845027f3834a704a20d1a20d2816b7a12b814107
Hanganu Augustin <augustin.hanganu@student.usv.ro>
2026-03-08 22:05:05 +0200

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,18 @@
+﻿int ore;
+double salariu;
+double tarif;
+Console.WriteLine("Introduceti numarul de ore lucrate:");
+ore = int.Parse(Console.ReadLine());
+Console.WriteLine("Introduceti tariful pe ora:");
+tarif = double.Parse(Console.ReadLine());   
+salariu = ore * tarif;
+Console.WriteLine("Salariul este: " + salariu);
+if (salariu > 3000)
+{
+    Console.WriteLine("Salariul este mare.");
+}
+else
+{
+    Console.WriteLine("Salariul este mic");
+}
+
